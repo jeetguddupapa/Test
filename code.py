a6be8e4d69f2da5db1d14c4ffadc607c073a655c
Hp <prasenjitbharshankar1>
2024-08-03 22:46:05 +0530

    second modification

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+#this is the new coomwwnt i adding for this code
+
 import urllib2
 BUFFER_SIZE = 256*1024
 url = 'http://downloads.sourceforge.net/project/pydev/pydev/PyDev%202.5.0/PyDev%202.5.0.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpydev%2Ffiles%2F&ts=1338118912&use_mirror=nchc'
